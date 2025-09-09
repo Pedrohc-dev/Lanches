57bf6bac5515f0f56142f21f68f90d93e55c75c9
P1Oliveira <pedrohc327@gmail.com>
2025-09-09 15:07:53 -0300

    Nova

---
+++ Lanches/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,29 @@
+﻿using Lanches.Models;
+using Lanches.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lanches.Components
+{
+    public class CarrinhoCompraResumo :ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhocompra)
+        {
+            _carrinhoCompra = carrinhocompra;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
