312bb9e07a88d869c0020493f7c647018f8cd9ac
Pedro <pedro.hcdo.info@gmail.com>
2025-09-11 23:01:00 -0300

    Mudancas

--- Lanches/Controllers/PedidoController.cs
+++ Lanches/Controllers/PedidoController.cs
@@ -23,7 +23,47 @@ namespace Lanches.Controllers
         [HttpPost]
         public IActionResult Checkout(Pedido pedido) 
         {
+            int totalItensPedido = 0;
+            decimal preocoTotalPedido = 0.0m;
+
+            //OBTEM OS ITENS DO CARRINHO DE COMPRA DO CLIENTE
+            List<CarrinhoCompraItem> items = _carrinhoCompra.GetCarrinhoCompraItems();
+            _carrinhoCompra.CarrinhoCompraItens = items;
+
+            //VERIFICA SE EXISTEM ITENS DE PEDIDO
+            if (_carrinhoCompra.CarrinhoCompraItens.Count == 0) 
+            {
+                ModelState.AddModelError("", "Seu carrinho esta vazio,que tal incluir um lanche...");
+            }
+
+            //CALCULAR O TOTAL DE ITENS E O TOTAL DO PEDIDO
+            foreach (var item in items) 
+            {
+                totalItensPedido += item.Quantidade;
+                preocoTotalPedido += (item.Lanche.Preco * item.Quantidade);
+            }
+
+            //ATRIBUIR OS VALORES OBTIDOS AO PEDIDO
+            pedido.TotalItensPedido = totalItensPedido;
+            pedido.PedidoTotal = preocoTotalPedido;
+
+            //VALIDA OS DADOS DO PEDIDO
+            if (ModelState.IsValid) 
+            {
+                //CRIAR OS PEDIDOS E OS DETALHES
+                _pedidoRepository.CriarPedido(pedido);
+
+                //DEFINE MENSAGENS AO CLIENTE
+                ViewBag.CheckoutCompletoMensagem = "Obrigado pelo seu pedido";
+                ViewBag.TotalPedido = _carrinhoCompra.GetCarrinhoCompraTotal();
+
+                //LIMPA CARRINHO DO CLIENTE
+                _carrinhoCompra.LimparCarrino();
+
+                //EXIBE A VIEW COM DADOS DO CLIENTE E DO PEDIDO
+                return View("~/Views/Pedido/CheckoutCompleto.cshtml", pedido);
+            }
             return View();
-        } 
+        }  
     }
 }
