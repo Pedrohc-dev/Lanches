65b08a75ded20ed3f3b59d25389b4f3beb67d3c1
P1Oliveira <P1Oliveira@schott.org>
2025-09-26 10:50:16 -0300

    Finalização do Projeto

--- Lanches/Program.cs
+++ Lanches/Program.cs
@@ -1,54 +1,118 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Lanches.Contex;
+using Lanches.Models;
+using Lanches.Repositories;
+using Lanches.Repositories.Interfaces;
+using Lanches.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Lanches.Services;
+using Lanches.Areas.Admin.Services;
+using ReflectionIT.Mvc.Paging;
+using FastReport.Data;
 
-namespace Lanches
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add builder.Services to the container.
+//opção var connection = builder.Configuration.GetConnectionString("DefaultConnection"); ai depois so referenciar a variavel no options
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+FastReport.Utils.RegisteredObjects.AddConnection(typeof(MsSqlDataConnection));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.Configure<ConfigurationImagens>(builder.Configuration.GetSection("ConfigurationPastaImagens"));
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<RelatorioVendasService>();
+builder.Services.AddScoped<GraficoVendasService>();
+
+builder.Services.AddAuthorization(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+    options.AddPolicy("Admin",
+        politica =>
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            politica.RequireRole("Admin");
+        });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-    }
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    app.UseHsts();
 }
 
-//var builder = WebApplication.CreateBuilder(args);
+app.UseHttpsRedirection();
+app.UseStaticFiles(); //Por a habilitação esta antes da UseAuthorization, todas as imagens statics Terão acesso publico
 
-// Add services to the container.
-//builder.Services.AddControllersWithViews();
+app.UseFastReport();
 
-//var app = builder.Build();
+app.UseRouting();
 
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
+CriarPerfisUsuario(app);
 
-//app.UseHttpsRedirection();
-//app.UseStaticFiles();
+app.UseSession();
 
-//app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
-//app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
 
-//app.MapControllerRoute(
-//    name: "default",
-//    pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapControllerRoute(
+     name: "areas",
+     pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}"
+     );
 
-//app.Run();
+    endpoints.MapControllerRoute(
+    name: "categoriaFiltro",
+    pattern: "Lanche/{action}/{categoria?}",
+    defaults: new { controller = "Lanche", action = "List" });
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+
+void CriarPerfisUsuario(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>(); //interface responsavel por criar instancia no meu scopo 
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        service.SeedRoles();//Cria os perfis
+        service.SeedUsers();//Cria os usuarios 
+    }
+}
