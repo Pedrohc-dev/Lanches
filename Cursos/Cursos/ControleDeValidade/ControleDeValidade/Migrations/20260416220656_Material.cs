f04b2e2a50f0dadbce57e90ce200e6e3304f05e2
Pedro <pedro.hcdo.info@gmail.com>
2026-04-16 19:07:42 -0300

    Migration Material

---
+++ Cursos/Cursos/ControleDeValidade/ControleDeValidade/Migrations/20260416220656_Material.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ControleDeValidade.Migrations
+{
+    /// <inheritdoc />
+    public partial class Material : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Quantidade",
+                table: "Almoxarifados",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Materiais",
+                columns: table => new
+                {
+                    MaterialId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CodigoMaterial = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materiais", x => x.MaterialId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Materiais");
+
+            migrationBuilder.DropColumn(
+                name: "Quantidade",
+                table: "Almoxarifados");
+        }
+    }
+}
