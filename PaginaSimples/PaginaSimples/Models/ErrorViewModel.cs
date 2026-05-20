ef0424eabad91eeb8c11c00990e3816a9bfcd817
Pedro.dev <136257860+Pedrohc-dev@users.noreply.github.com>
2026-05-20 10:06:27 -0300

    Delete PaginaSimples directory

--- PaginaSimples/PaginaSimples/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PaginaSimples.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- PaginaSimples/PaginaSimples/wwwroot/Instalar_Impressoras.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-start \\sbrri1s0002\COLORIDA
-
-start \\sbrri1s0002\PRETO_BRANCO
-
-
-
-
